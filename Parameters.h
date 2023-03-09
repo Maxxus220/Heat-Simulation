db2803ff4513cbf1607539a1e56bcc3fcb9f135c
Maxxus220 <Maxxus220@gmail.com>
2023-03-09 11:59:34 -0600

    Added documentation for Parameters.h

--- Parameters.h
+++ Parameters.h
@@ -1,9 +1,18 @@
+/**
+ * @file Parameters.hj
+ * @brief Contains definitions for parameters of HeatSim
+*/
+
 #define XDIM 15
 #define YDIM 15
 #define ZDIM 15
 
 #define OPACITY 0.2
 
-// #define DEFAULT_COLOR vec4(0.1, 0, 0, 1)
+/**
+ * @def DEFAULT_COLOR
+ * @brief The default color of simulation cubes
+*/
 #define DEFAULT_COLOR vec4(0, 0, 0, 1)
+
 #define BACKGROUND_COLOR vec4(0.3, 0.3, 0.3, 1)
